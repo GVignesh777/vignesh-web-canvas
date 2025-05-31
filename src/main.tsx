1e06a28ea1be95ae1162ff87c3e0d18c2049c99b
GVignesh777 <gokamvignesh9885558704@gmail.com>
2025-05-31 14:01:13 +0530

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx' // Assuming your main component is in App.jsx
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
